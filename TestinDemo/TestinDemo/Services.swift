8efc5c102c5904c1a906b1fd3a7d00fd9e7ab6cf
izooto-mobile-sdk <github@datability.co>
2020-02-07 12:27:30 +0530

    First

---
+++ TestinDemo/TestinDemo/Services.swift
@@ -0,0 +1,17 @@
+//
+//  Services.swift
+//  TestinDemo
+//
+//  Created by Amit on 07/02/20.
+//  Copyright © 2020 Amit. All rights reserved.
+//
+
+import Foundation
+public class Services
+{
+    public init(){}
+    public static func ShowMessage()->String
+    {
+        return "Show Message Testing Amit Kumar"
+    }
+}
---
+++ TestinDemo/TestinDemo/TestinDemo.h
@@ -0,0 +1,19 @@
+//
+//  TestinDemo.h
+//  TestinDemo
+//
+//  Created by Amit on 07/02/20.
+//  Copyright © 2020 Amit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestinDemo.
+FOUNDATION_EXPORT double TestinDemoVersionNumber;
+
+//! Project version string for TestinDemo.
+FOUNDATION_EXPORT const unsigned char TestinDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestinDemo/PublicHeader.h>
+
+
